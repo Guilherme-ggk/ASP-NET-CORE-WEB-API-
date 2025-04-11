92e16166f1e8309e23a32f5668ebcd0c7a89a4ef
Gui-gt <ggk0830755@gmail.com>
2025-04-11 19:26:56 -0300

    att

--- WebApiRestFul/obj/Debug/net8.0/WebApiRestFul.AssemblyInfo.cs
+++ WebApiRestFul/obj/Debug/net8.0/WebApiRestFul.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiRestFul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006c20a7512846a656832541807dd73c82057b28")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiRestFul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiRestFul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
